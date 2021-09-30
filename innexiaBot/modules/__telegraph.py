e7f4adaa1dc36377f2fa4a889429287a32f759fc
ROCKSTAR OP <88956892+T5FR0CK5T4R@users.noreply.github.com>
2021-09-30 19:11:30 +0530

    Update __telegraph.py

--- innexiaBot/modules/__telegraph.py
+++ innexiaBot/modules/__telegraph.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-Sammy = "Innexia"
+Rockstar = "Florenza"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Sammy)
 auth_url = r["auth_url"]
